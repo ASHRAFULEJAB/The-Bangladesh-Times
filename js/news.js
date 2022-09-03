9e4c296dec7a727cf0dba0e2f789428f484bcb28
Md.Ashraful Islam Khan <19103189@iubat.edu>
2022-09-04 05:30:20 +0600

    footer-part

--- js/news.js
+++ js/news.js
@@ -9,6 +9,7 @@ const setAllCategoriesName= async()=>{
       
         const data1 =await loadAllCategories();
         
+        
         const category = document.getElementById('all-categories'); 
                
         for( const news of data1){
@@ -21,9 +22,7 @@ const setAllCategoriesName= async()=>{
             category.appendChild(li);
             
         }
-        
-        
-        
+   
 }
 
 const getNewsDetails=async(category_id)=>{
@@ -35,11 +34,13 @@ const getNewsDetails=async(category_id)=>{
     newsData=data.data
     // console.log(newsData);
     newsLoading (newsData);
+    return (newsData);
     
 }
 
 const newsLoading = (news)=>{
     // console.log(news)
+    // return {_id}=news;
     const categoryNews = document.getElementById('new-details'); 
 
     const newsCount = document.getElementById('news-count');
@@ -52,20 +53,19 @@ const newsLoading = (news)=>{
      if(newNewsCount > 0){
 
           newsCount.value= newNewsCount + 'News Found'
-
      }
      else if(newNewsCount <=0){
          newsCount.value = 'No News Found'
-       
      }
      
      
     categoryNews.textContent='';
+    // const newsDetailsModal = document.getElementById('newsDetailsModalLabel');
     news.forEach(newsInfo =>{
-        
+        // console.log(newsInfo)
         const newsSection = document.createElement('div');
                 newsSection.classList.add('col');
-                const {thumbnail_url,title,details,author,total_view}=newsInfo;
+                const {thumbnail_url,title,details,author,total_view,image_url}=newsInfo;
                 newsSection.innerHTML = `
                            
                 <div class="card">
@@ -78,19 +78,45 @@ const newsLoading = (news)=>{
                   <img  width="40" height="40"class="rounded-circle me-5"  src="${author.img}"/>
                   <p> ${total_view ?total_view : 'No Data Found'}</p>
                   </div>
-                  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newsDetailsModal"> Show Details</button>
+                  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newsDetailsModal"
+                   onclick="displayNewsModal('${title}','${image_url}')"> Show Details</button>
         
                   </div>
-                           
                            
                 `;
                 categoryNews.appendChild(newsSection,newsCount);
 
             })
             toggleSpinner(false);
+
+            // const {id}=news;
+            // console.log(id)
+
 }
 
+// const newsLoadModal = async ()=>{
+//     const newsData =  await getNewsDetails();
+//     console.log(newsData)
+    // const url = `https://openapi.programming-hero.com/api/news/${id}`;
+    // console.log(newsData)
+//     const res = await fetch(url);
+//     const data = res.json();
+//     const dataNews = data._id;
+//     displayNewsModal (dataNews)
+// }
 
+ const displayNewsModal = (title,image_url,details,total_view)=>{
+    // console.log(details,total_view)
+    const modalTitle = document.getElementById('newsDetailsModalLabel');
+    modalTitle.innerText=title;
+    const newsModalDetails = document.getElementById('news-modal-details');
+   newsModalDetails.innerHTML = `
+     
+   <img class="img-fluid" src="${image_url}"/>
+   `;
+   
+ }
+ 
 
 // spinner
 
@@ -105,76 +131,10 @@ const toggleSpinner = isLoading =>{
 }
 
 
-
-
-
-
-// const setAllNews= async()=>{
-
-//     const newsData =await getNewsDetails();
-//     console.log(newsData)
-//     // const categoryNews = document.getElementById('new-details');        
-//     for( const newNews of newsData){
-//         // console.log(newNews);
-//         const newsSection = document.createElement('div');
-//         newsSection.classList.add('col')
-//         newsSection.innerHTML = `
-                   
-//         <div class="card">
-//         <img src="${newNews.thumbnail_url}" class="card-img-fluid" alt="...">
-//         <div class="card-body">
-//           <h5 class="card-title">${newNews.title ? newNews.title : 'No Name Found'  }</h5>
-//           <p class="card-text">${newNews.details.slice(0,100)}...</p>
-//           <div class="d-flex p-2">
-//           <p class="me-5"> ${newNews.author.name ?newNews.author.name : 'No Data Found' }</p>
-//           <img  width="40" height="40"class="rounded-circle me-5"  src="${newNews.author.img}"/>
-//           <p> ${newNews.total_view ?newNews.total_view : 'No Data Found'}</p>
-//           </div>
-//           <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newsDetailsModal"> Show Details</button>
-
-//           </div>
-                   
-                   
-//         `;
-//         categoryNews.appendChild(newsSection);
-//     }
-    
-    
-// }
 getNewsDetails()
-// document.getElementById('all-categories').addEventListener('click', async function(category_id){
-//     const abd=categoryDetails(category_id)
-//     console.log(abd)
-// })
 
-// const categoryDetails=async(category_id) =>{
-//     const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`
-//     console.log(url)
-//     const res = await fetch(url);
-//     const data = await res.json();
-//     console.log(data)
-// }
-
-// const newsSearch = document.getElementById('all-categories');
-
-// newsSearch.addEventListener('click',async(event)=>{
-//     const allNewsSearch = await loadAllCategories()
-//     console.log(allNewsSearch)
-//     // for(const news of allNewsSearch)
-//     // console.log(news.category_id)
-// })
-
-
-// const loadNews = async(category_id)=>{
-//     const url =`https://openapi.programming-hero.com/api/news/category/${category_id}`
-//     const res = await fetch(url);
-//     const data = await res.json();
-//     newsData=data.data
-//     return(newsData);
-// }
-
-// setAllNews()
 
 setAllCategoriesName()
 
 
+ 
